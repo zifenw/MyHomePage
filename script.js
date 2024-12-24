193eea3b5233178a3153c6aa906b4aae4fe822a2
zifenw <zifenw@uw.edu>
2024-12-24 14:17:48 -0800

    Merry Christmas

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for (let i = 0; i< navbarItem.length; i++){
 }
 
 var typed = new Typed(".multiple-text", {
-    strings: ['Zifeng Wang','[ˌtsiːˈfʌŋ wæŋ]','UW CS Undergraduate', 'Full-Stuck Developer'],
+    strings: ['Merry Christmas','Zifeng Wang','[ˌtsiːˈfʌŋ wæŋ]','UW CS Undergraduate', 'Full-Stuck Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
